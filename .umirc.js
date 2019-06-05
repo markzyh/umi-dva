059d3336c66f5cb47438fc64be3081e15cb5768f
Markzyh <980511383@qq.com>
2019-06-05 18:03:23 +0800

    init

---
+++ .umirc.js
@@ -0,0 +1,23 @@
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    [
+      'umi-plugin-react',
+      {
+        antd: false,
+        dva: {
+          immer: true,
+        },
+        dynamicImport: false,
+        title: 'umi-loan',
+        dll: false,
+
+        routes: {
+          exclude: [/components\//],
+        },
+      },
+    ],
+  ],
+};
